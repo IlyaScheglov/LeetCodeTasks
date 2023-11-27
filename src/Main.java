1165d3c0564fd778216fd9eed8d960ead3641f30
ilya <ilasglv@gmail.com>
2023-11-27 21:11:53 +0300

    3 new tasks

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        String answer = appendNewNumsToBuilder(4);
+        String answer = countAndSay(4);
         System.out.println(answer);
     }
 
